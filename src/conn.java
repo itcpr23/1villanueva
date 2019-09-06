d037bec1331ecab3d6da195e363337dbdc23e31c
hp <hp@Traci>
2019-09-06 14:48:43 +0800

    init commit

---
+++ src/conn.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class conn {
+    protected String dbname = "villanueva_reg";
+    protected String url = "jdbc:mysql://localhost/"+dbname;
+     protected String username = "root";
+     protected String password = ""; 
+    
+}
