94e7f53b6f5b0fd70d2f810b7e8217cb5d544329
hp <villanuevatraci56@gmail.com>
2019-09-22 21:48:52 +0800

    init commit

--- src/productframe.java
+++ src/productframe.java
@@ -2,6 +2,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,10 +30,9 @@ try{
         ps.setInt(2, prqty);
         ps.setInt(3, prprice);
         ps.executeUpdate();
-}       catch (SQLException ex) {
+}       catch (ClassNotFoundException ex) {
             Logger.getLogger(productframe.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
+        } catch (SQLException ex) {
             Logger.getLogger(productframe.class.getName()).log(Level.SEVERE, null, ex);
         }
-
 
